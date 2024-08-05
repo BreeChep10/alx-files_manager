7a8684c107c8c950f6f687f7995739b4ff84e7da
Bridgit Chepkwony <bridgitchepkwony3@gmail.com>
2024-08-05 20:01:10 +0300

    corrected file on comments

--- utils/redis.js
+++ utils/redis.js
@@ -1,4 +1,5 @@
-const redis = require('redis');
+import { promisify } from 'util';
+import { createClient } from 'redis';
 
 /**
  * Represents a Redis client.
@@ -8,32 +9,32 @@ class RedisClient {
    * Creates a new RedisClient instance.
    */
   constructor() {
-    this.client = redis.createClient();
+    this.client = createClient();
+    this.isClientConnected = true;
     this.client.on('error', (err) => {
-      console.error(`Redis client error: ${err}`);
+      console.error('Redis client failed to connect:', err.message || err.toString());
+      this.isClientConnected = false;
+    });
+    this.client.on('connect', () => {
+      this.isClientConnected = true;
     });
   }
 
   /**
-   * Checks if this client is alive.
-   * @returns {boolean} True if the client is connected, false otherwise.
+   * Checks if this client's connection to the Redis server is active.
+   * @returns {boolean}
    */
   isAlive() {
-    return this.client.connected;
+    return this.isClientConnected;
   }
 
   /**
    * Retrieves the value of a given key.
    * @param {String} key The key of the item to retrieve.
-   * @returns {Promise<String>} The value of the key.
+   * @returns {String | Object}
    */
   async get(key) {
-    return new Promise((resolve, reject) => {
-      this.client.get(key, (err, value) => {
-        if (err) return reject(err);
-        resolve(value);
-      });
-    });
+    return promisify(this.client.GET).bind(this.client)(key);
   }
 
   /**
@@ -44,12 +45,8 @@ class RedisClient {
    * @returns {Promise<void>}
    */
   async set(key, value, duration) {
-    return new Promise((resolve, reject) => {
-      this.client.setex(key, duration, value, (err) => {
-        if (err) return reject(err);
-        resolve();
-      });
-    });
+    await promisify(this.client.SETEX)
+      .bind(this.client)(key, duration, value);
   }
 
   /**
@@ -58,12 +55,7 @@ class RedisClient {
    * @returns {Promise<void>}
    */
   async del(key) {
-    return new Promise((resolve, reject) => {
-      this.client.del(key, (err) => {
-        if (err) return reject(err);
-        resolve();
-      });
-    });
+    await promisify(this.client.DEL).bind(this.client)(key);
   }
 }
 
