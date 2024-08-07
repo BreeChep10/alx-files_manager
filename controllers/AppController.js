7d2abe01f4c437420e7d968f4585703674e1537e
Bridgit Chepkwony <bridgitchepkwony3@gmail.com>
2024-08-07 15:57:35 +0300

    correcting AppController file

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,37 +1,31 @@
-import dbClient from '../utils/db';
-import redisClient from '../utils/redis';
+const redisClient = require('../utils/redis');
+const dbClient = require('../utils/db');
 
 class AppController {
-  /**
-   * Controller for endpoint GET /status that retrieves
-   * mongodb client and redis client connection status
-   * @typedef {import("express").Request} Request
-   * @typedef {import("express").Response} Response
-   * @param {Request} _req - request object
-   * @param {Response} res  - response object
-   */
-  static getStatus(_req, res) {
-    if (dbClient.isAlive() && redisClient.isAlive()) {
-      res.status(200).json({ redis: true, db: true });
-    }
+/**
+ * Controller for endpoint GET /status that retrieves
+ * mongodb client and redis client connection status
+ * @typedef {import("express").Request} Request
+ * @typedef {import("express").Response} Response
+ * @param {Request} _req - request object
+ * @param {Response} res  - response object
+ */
+
+  static getStatus(req, res) {
+    res.status(200).json({
+      redis: redisClient.isAlive(),
+      db: dbClient.isAlive()
+    });
   }
 
-  /**
-   * Controller for endpoint GET /stats that retrieves
-   * count of users and files
-   * @param {Request} _req - Request object
-   * @param {Response} res  - Response object
-   * @param {import("express").NextFunction} next - Next function
-   */
-  static async getStats(_req, res, next) {
-    try {
-      const users = await dbClient.nbUsers();
-      const files = await dbClient.nbFiles();
-      res.status(200).json({ users, files });
-    } catch (err) {
-      next(err);
-    }
+  static async getStats(req, res) {
+    const usersCount = await dbClient.nbUsers();
+    const filesCount = await dbClient.nbFiles();
+    res.status(200).json({
+      users: usersCount,
+      files: filesCount
+    });
   }
 }
 
-export default AppController;
+module.exports = AppController;
