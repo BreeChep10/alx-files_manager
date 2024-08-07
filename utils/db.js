c39b5e8ec1255466865508653c6d17c1f7027c20
Bridgit Chepkwony <bridgitchepkwony3@gmail.com>
2024-08-07 14:14:35 +0300

    working on th eserver side logic

--- utils/db.js
+++ utils/db.js
@@ -1,44 +1,58 @@
 import { MongoClient } from 'mongodb';
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 class DBClient {
+  /**
+   * Initializes a new instance of DBClient
+   */
   constructor() {
-    const host = process.env.DB_HOST || 'localhost';
-    const port = process.env.DB_PORT || 27017;
-    const database = process.env.DB_DATABASE || 'files_manager';
-
-    const url = `mongodb://${host}:${port}`;
-    this.client = new MongoClient(url, { useUnifiedTopology: true });
-    this.db = null;
-
-    this.client.connect((err) => {
-      if (err) {
-        console.error('Failed to connect to MongoDB', err);
-        return;
-      }
-      this.db = this.client.db(database);
-      console.log('Connected to MongoDB');
+    const HOST = process.env.DB_HOST || 'localhost';
+    const PORT = process.env.BD_PORT || 27017;
+    const DATABASE = process.env.DB_DATABASE || 'files_manager';
+    const URI = `mongodb://${HOST}:${PORT}`;
+    this.mongoClient = new MongoClient(URI, { useUnifiedTopology: true });
+    this.mongoClient.connect((error) => {
+      if (!error) this.db = this.mongoClient.db(DATABASE);
     });
   }
 
+  /**
+   * Check mongodb client's connection status
+   * @returns {boolean} mongoClient connection status
+   */
   isAlive() {
-    return !!this.db;
+    return this.mongoClient.isConnected();
+  }
+
+  /**
+   * Retrieves specified collection from database
+   * @returns {import("mongodb").Collection} - users collection object
+   */
+  getCollection(collectionName) {
+    const collection = this.db.collection(collectionName);
+    return collection;
   }
 
   async nbUsers() {
-    if (!this.isAlive()) {
-      return 0;
-    }
-    return this.db.collection('users').countDocuments();
+    const usersCollection = this.getCollection('users');
+    const numberOfUsers = await usersCollection.countDocuments();
+    return numberOfUsers;
   }
 
+  /**
+   * Queries 'files' collection
+   * @returns {number} - number of documents in files collection
+   */
   async nbFiles() {
-    if (!this.isAlive()) {
-      return 0;
-    }
-    return this.db.collection('files').countDocuments();
+    const filesCollection = this.getCollection('files');
+    const numberOfFiles = filesCollection.countDocuments();
+    return numberOfFiles;
+  }
+
+  /**
+   * Closes connection to mongodb client
+   */
+  async close() {
+    await this.mongoClient.close();
   }
 }
 
